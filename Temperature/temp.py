15d182ea32096439423f348024dab38b9a46532d
elvaigh <celvaigh@enssat.fr>
2017-04-26 16:44:53 +0200

    Update temp.py

--- Temperature/temp.py
+++ Temperature/temp.py
@@ -5,3 +5,11 @@ for j in list(map(int,input().split())):
  k=abs(j)
  if k<m or(k==m and j>0):m,o=k,j 
 print(o)
+
+#Methode 2
+"""o=input()
+m=900
+for j in [int(i)for i in input().split()]:
+ k=abs(j)
+ if k<m or(k==m and j>0):m,o=k,j 
+print(o)"""
